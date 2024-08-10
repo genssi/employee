ad2c1e8f729ce17664d6a4c9d81dda4cd2095ad0
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2024-08-10 08:04:52 +0600

    completed all

--- src/components/employes-add-form/employes-add-form.js
+++ src/components/employes-add-form/employes-add-form.js
@@ -1,27 +1,69 @@
-import './employes-add-form.css'
+import { Component } from "react";
 
-const EmployesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form className="add-form d-flex">
-                <input
-                    type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?"
-                />
-                <input
-                    type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?"
-                />
+import "./employes-add-form.css";
 
-                <button type="submit" className="btn btn-outline-light">
-                    Добавить
-                </button>
-            </form>
-        </div>
-    );
-};
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            salary: "",
+        };
+    }
 
-export default EmployesAddForm;
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        
+        if (this.state.name <= 0 || this.state.name === "" || this.state.salary === "" || this.state.salary < 0) {
+            alert("Имя и Зарплата не должны быть пустыми или отрицательными числами!");
+            return;
+        }
+
+        this.props.onAdd(this.state.name, this.state.salary);
+
+        this.setState({
+            name: "",
+            salary: "",
+        });
+    };
+
+    render() {
+        const { name, salary } = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form className="add-form d-flex" onSubmit={this.onSubmit}>
+                    <input
+                        type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name="name"
+                        value={name}
+                        onChange={this.onValueChange}
+                    />
+                    <input
+                        type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?"
+                        name="salary"
+                        value={salary}
+                        onChange={this.onValueChange}
+                    />
+
+                    <button type="submit" className="btn btn-outline-light">
+                        Добавить
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default EmployeesAddForm;
--- src/components/employes-list/employes-list.js
+++ src/components/employes-list/employes-list.js
@@ -1,14 +1,25 @@
 import EmployesListItem from "../employes-list-item/employes-list-item";
 import './employes-list.css'
 
-const EmployesList = () => {
+const EmployesList = ({data, onDelete, onToggleProp}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployesListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+            />
+        );
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployesListItem />
-            <EmployesListItem />
-            <EmployesListItem />
+            {elements}
         </ul>
     );
-};
+}; 
 
 export default EmployesList;
